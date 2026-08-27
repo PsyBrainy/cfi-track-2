f3b6b4a6556a9f65d09ce0be3adccc113360c655
Elias Lambrecht <lmelias95@hotmail.com>
2026-08-27 20:04:04 -0300

    feat: jwtauthentificationfilter y authorization con formato bearer hecho

--- alkemy/src/main/java/com/wallet/alkemy/models/tableUser.java
+++ alkemy/src/main/java/com/wallet/alkemy/models/tableUser.java
@@ -1,18 +1,29 @@
 package com.wallet.alkemy.models;
 
+import com.wallet.alkemy.user.domain.tableUserRole;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.math.BigInteger;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity (name = "Users")
 @Table (name = "Users")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 
-public class tableUser {
+public class tableUser implements UserDetails {
 
     @Id
     @Column (name = "id")
@@ -70,5 +81,40 @@ public class tableUser {
     @Column (name = "Password")
     private String password;
 
+    @Column (name = "Role")
+    @Enumerated(EnumType.STRING)
+    private tableUserRole role;
 
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override  // Aca se asigna el Email como nombre de usuario
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
